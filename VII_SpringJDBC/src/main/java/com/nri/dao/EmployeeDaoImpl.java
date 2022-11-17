d9a2ce77a7a3603a50019577b36cb8acecb4a71a
manshiginodia <manshiginodia@gmail.com>
2022-11-17 16:51:01 +0530

    17/11

--- VII_SpringJDBC/src/main/java/com/nri/dao/EmployeeDaoImpl.java
+++ VII_SpringJDBC/src/main/java/com/nri/dao/EmployeeDaoImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import com.nri.entities.Employee;
 
+@Component
 public class EmployeeDaoImpl implements EmployeeDao {
 
 	@Autowired
